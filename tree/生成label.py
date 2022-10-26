c275f525afca281a32308922858a4f57823a6d88
Q <2398627848@qq.com>
2022-10-26 20:00:01 +0800

    2022-10-26

---
+++ tree/生成label.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+# 读取的文件是DataFarame格式
+train_file = pd.read_csv('D:/PycharmProjects/learning/tree/treeData/train.csv')
+# 将文件中label这一列拿出来 把格式转换为numpy格式
+data = train_file['label'].values
+# 去除重复项，转换为list形式
+data_list = list(set(data))
+dict = {}
+for i in range(len(data_list)):
+    # 构造映射字典
+    dict[data_list[i]] = i
+# 利用映射将字符串类别转换为数字
+train_file['label_number'] = train_file['label'].map(dict)
+# 将修改的文件保存到本地 不保存序号
+train_file.to_csv('D:/PycharmProjects/learning/tree/treeData/train_.csv', index=False)
